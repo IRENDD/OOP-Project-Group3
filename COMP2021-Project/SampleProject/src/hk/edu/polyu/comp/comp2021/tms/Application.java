11de9781ecebff68e2a72d55f368fe1ace389b9c
tahminanower <85175021+tahminanower@users.noreply.github.com>
2023-11-13 23:00:26 +0800

    first try

--- COMP2021-Project/SampleProject/src/hk/edu/polyu/comp/comp2021/tms/Application.java
+++ COMP2021-Project/SampleProject/src/hk/edu/polyu/comp/comp2021/tms/Application.java
@@ -2,13 +2,83 @@ package hk.edu.polyu.comp.comp2021.tms;
 
 import hk.edu.polyu.comp.comp2021.tms.model.TMS;
 
-public class Application {
+package hk.edu.polyu.comp.comp2021.tms;
+import java.util.*;
+import hk.edu.polyu.comp.comp2021.tms.model.Task;
 
-    public static void main(String[] args){
-        TMS tms = new TMS();
-        // Initialize and run the system
-        // hello this is something new
-        // i am Tahmin
+import java.util.*;
+import java.util.stream.Collectors; 
+
+public class Application{
+    Map<String, Task> taskMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        Task tms = new Task();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter a command (CreateSimpleTask or CreateCompositeTask):");
+            String input = scanner.nextLine();
+
+            if (input.startsWith("CreateSimpleTask")) {
+                String[] tokens = input.split(" ");
+                if (tokens.length >= 5) {
+                    String name = tokens[1];
+                    String description = tokens[2];
+                    double duration = Double.parseDouble(tokens[3]);
+                    List<String> prerequisites = Arrays.asList(tokens[4].split(","));
+                    tms.createSimpleTask(name, description, duration, prerequisites);
+                } else {
+                    System.out.println("Invalid CreateSimpleTask command format.");
+                }
+            } else if (input.startsWith("CreateCompositeTask")) {
+                String[] tokens = input.split(" ");
+                if (tokens.length >= 4) {
+                    String name = tokens[1];
+                    String description = tokens[2];
+                    String[] subtaskNames = tokens[3].split(",");
+                    tms.createCompositeTask(name, description, subtaskNames);
+                } else {
+                    System.out.println("Invalid CreateCompositeTask command format.");
+                }
+            } else if (input.equalsIgnoreCase("exit")) {
+                break;
+            } else {
+                System.out.println("Unknown command.");
+            }
+        }
+
+        scanner.close();
     }
 
+    public void createSimpleTask(String name, String description, double duration, List<String> prerequisites) {
+        if (!taskMap.containsKey(name)) {
+            Task task = new Task(name, description, duration, prerequisites);
+            taskMap.put(name, task);
+            System.out.println("Simple task created: " + name);
+        } else {
+            System.out.println("Task with the same name already exists: " + name);
+        }
+    }
+
+    public void createCompositeTask(String name, String description, String[] subtaskNames) {
+        if (!taskMap.containsKey(name)) {
+            List<String> subtaskList = Arrays.asList(subtaskNames);
+            Task task = new Task(name, description, 0.0, subtaskList); // Duration is 0 for composite tasks
+            taskMap.put(name, task);
+            System.out.println("Composite task created: " + name);
+
+            for (String subtaskName : subtaskNames) {
+                if (!taskMap.containsKey(subtaskName)) {
+                    System.out.println("Subtask not found: " + subtaskName);
+                    taskMap.remove(name); // Remove the composite task if any subtask is missing
+                    return;
+                }
+            }
+        } else {
+            System.out.println("Task with the same name already exists: " + name);
+        }
+    }
 }
+
+
--- COMP2021-Project/SampleProject/src/hk/edu/polyu/comp/comp2021/tms/model/TMS.java
+++ COMP2021-Project/SampleProject/src/hk/edu/polyu/comp/comp2021/tms/model/TMS.java
@@ -1,7 +1,54 @@
 package hk.edu.polyu.comp.comp2021.tms.model;
+import java.util.*;
+
 
 public class TMS {
+    String name;
+    String description;
+    double duration;
+    List<String> prerequisites;
 
-    public TMS(){}
+    public TMS(){
+
+    }
+    public void Task(String name, String description, double duration, List<String> prerequisites) {
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.prerequisites = prerequisites;
+    }
+
+    Map<String, Task> taskMap = new HashMap<>();
+
+    public void createSimpleTask(String name, String description, double duration, List<String> prerequisites) {
+        if (!taskMap.containsKey(name)) {
+            Task task = new Task(name, description, duration, prerequisites);
+            taskMap.put(name, task);
+            System.out.println("Simple task created: " + name);
+        } else {
+            System.out.println("Task with the same name already exists: " + name);
+        }
+    }
+
+    public void createCompositeTask(String name, String description, String[] subtaskNames) {
+        if (!taskMap.containsKey(name)) {
+            List<String> subtaskList = Arrays.asList(subtaskNames);
+            Task task = new Task(name, description, 0.0, subtaskList); // Duration is 0 for composite tasks
+            taskMap.put(name, task);
+            System.out.println("Composite task created: " + name);
+
+            for (String subtaskName : subtaskNames) {
+                if (!taskMap.containsKey(subtaskName)) {
+                    System.out.println("Subtask not found: " + subtaskName);
+                    taskMap.remove(name); // Remove the composite task if any subtask is missing
+                    return;
+                }
+            }
+        } else {
+            System.out.println("Task with the same name already exists: " + name);
+        }
+    }
+
+
 
 }
